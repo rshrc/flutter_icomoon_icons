981f1f044ad46d6601189371ead5073295f26767
Rishi Banerjee <rishieric91@gmail.com>
2019-07-18 06:26:08 +0530

    Updated example

--- README.md
+++ README.md
@@ -6,13 +6,11 @@
 
 facing any problems? have a look into the documentation (class IcoMoonIcons)
 
-## 222 Weather Themed Icons for Flutter.
+## 490 Free IcoMoon Icons for Flutter.
 
 This flutter package allows you to use all of the IcoMoon package
 
 Find it at at [pub.dartlang.org](https://pub.dev/packages/flutter_icomoon_icons)
-
-![Icon Preview](https://i.imgur.com/XmZW2q3.png)
 
 ## Installation
 
@@ -28,10 +26,10 @@ import 'package:flutter_icomoon_icons/flutter_icomoon_icons.dart';
 class MyAwesomeWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return IconButton(
-      // Use the EvaIcons class for the IconData
-      icon: Icon(IcoMoonIcons.wiDaySunny),
+      // Use the IcoMoonIcons class for the IconData
+      icon: Icon(IcoMoonIcons.home),
       onPressed: () {
-          print("IcoMoon Icon Pressed! It's Sunny");
+          print("IcoMoon Icon Pressed! It's Home!");
       }
      );
   }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,15 +7,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   Widget iconCard(String iconName, IconData iconData) {
     return Card(
+      color: Colors.blueGrey,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Icon(
             iconData,
             size: 50.0,
+            color: Colors.white,
           ),
           SizedBox(height: 30.0),
-          Text(iconName)
         ],
       ),
     );
@@ -29,8 +30,10 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color(0xFFF9F9F9),
         appBar: AppBar(
-          title: Text('IcoMoonIcons Example'),
+          title: Text('IcoMoonIcons Example', style: TextStyle(color: Colors.blueGrey)),
           centerTitle: true,
+          elevation: 0.0,
+          backgroundColor: Colors.white,
         ),
         body: GridView.builder(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
